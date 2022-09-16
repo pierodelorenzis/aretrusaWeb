9d6a57521395e497867142700a5cd1298df518f0
Matteo <matteodmt26@gmail.com>
2022-09-16 11:34:41 +0200

    Add view genre

---
+++ src/main/java/com/example/aretrusaWeb/view/UIGenre.java
@@ -0,0 +1,22 @@
+package com.example.aretrusaWeb.view;
+
+public class UIGenre {ù
+
+    public String name;
+
+    public UIGenre (){
+
+    }
+
+    public UIGenre(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
