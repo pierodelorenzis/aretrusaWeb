a2ecea3c51dffb65b2331df81f883a1de39efb8f
fedepe00 <federico9b@libero.it>
2022-09-16 11:29:29 +0200

    Bookcase,Aisle,Feedback aggiunta Ui

---
+++ src/main/java/com/example/aretrusaWeb/view/UiAisle.java
@@ -0,0 +1,21 @@
+package com.example.aretrusaWeb.view;
+
+public class UiAisle {
+
+    public int aisle;
+
+    public UiAisle() {
+    }
+
+    public UiAisle(int aisle) {
+        this.aisle = aisle;
+    }
+
+    public int getAisle() {
+        return aisle;
+    }
+
+    public void setAisle(int aisle) {
+        this.aisle = aisle;
+    }
+}
