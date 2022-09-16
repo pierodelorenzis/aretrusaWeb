974d5f413f280ea2d08d685fdfb6ab6c1aaa71e2
Christian Paolicelli <paolicelli_c@libero.it>
2022-09-16 12:11:32 +0200

    Customer e StateofBorrow UI

--- src/main/java/com/example/aretrusaWeb/view/UiCustomer.java
+++ src/main/java/com/example/aretrusaWeb/view/UiCustomer.java
@@ -1,5 +1,7 @@
 package com.example.aretrusaWeb.view;
 
+import com.example.aretrusaWeb.model.Customer;
+
 public class UiCustomer extends UiUser {
 
     public float balance;
@@ -8,12 +10,12 @@ public class UiCustomer extends UiUser {
     public int bookDamage;
     public int evaluation;
 
-    public UiCustomer(float balance, int bookLost, int bookDelay, int bookDamage, int evaluation) {
-        this.balance = balance;
-        this.bookLost = bookLost;
-        this.bookDelay = bookDelay;
-        this.bookDamage = bookDamage;
-        this.evaluation = evaluation;
+    public UiCustomer(Customer customer ) {
+        this.balance = customer.getBalance();
+        this.bookLost = customer.getBookLost();
+        this.bookDelay = customer.getBookDelay();
+        this.bookDamage = customer.getBookDamage();
+        this.evaluation = customer.getEvaluation();
     }
 
     public UiCustomer() {
