06a911fb3c01a7f3cd6a0cdb44495079cf20343b
fedepe00 <federico9b@libero.it>
2022-09-16 10:49:05 +0200

    Bookcase,Aisle,Feedback

---
+++ src/main/java/com/example/aretrusaWeb/model/Aisle.java
@@ -0,0 +1,19 @@
+package com.example.aretrusaWeb.model;
+
+public class Aisle {
+
+    private int aisle;
+
+
+    public Aisle(int aisle) {
+        this.aisle = aisle;
+    }
+
+    public int getAisle() {
+        return aisle;
+    }
+
+    public void setAisle(int aisle) {
+        this.aisle = aisle;
+    }
+}
