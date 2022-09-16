c06cfb4e7834e8a3e1d489be0212aac6e735cd5b
pierodelorenzis <delorenzispiero@gmail.com>
2022-09-16 12:17:25 +0200

    update UiBook

--- src/main/java/com/example/aretrusaWeb/view/UiBook.java
+++ src/main/java/com/example/aretrusaWeb/view/UiBook.java
@@ -1,5 +1,7 @@
 package com.example.aretrusaWeb.view;
 
+import com.example.aretrusaWeb.model.Book;
+
 import java.util.Date;
 
 public class UiBook {
@@ -8,7 +10,7 @@ public class UiBook {
     public String isbn;
     public int quantity;
     public String description;
-    public Date year;
+    public int year;
     public int page;
     public int pegi;
     public float price;
@@ -16,108 +18,57 @@ public class UiBook {
     public boolean sell;
     public int numBorrow;
 
-    public UiBook() {
+    public UiBook()
+    {
+
     }
 
-    public UiBook(String name, String isbn, int quantity, String description, Date year, int page, int pegi, float price, float currentPrice, boolean sell, int numBorrow) {
-        this.name = name;
-        this.isbn = isbn;
-        this.quantity = quantity;
-        this.description = description;
-        this.year = year;
-        this.page = page;
-        this.pegi = pegi;
-        this.price = price;
-        this.currentPrice = currentPrice;
-        this.sell = sell;
-        this.numBorrow = numBorrow;
+    public UiBook(Book book) {
+        this.name = book.getName();
+        this.isbn = book.getIsbn();
+        this.quantity = book.getQuantity();
+        this.description = book.getDescription();
+        this.year = book.getYear();
+        this.page = book.getPage();
+        this.pegi = book.getPegi();
+        this.price = book.getPrice();
+        this.currentPrice = book.getCurrentPrice();
+        this.sell = book.isSell();
+        this.numBorrow = book.getNumBorrow();
     }
 
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getIsbn() {
         return isbn;
     }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
     public int getQuantity() {
         return quantity;
     }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
     public String getDescription() {
         return description;
     }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Date getYear() {
+    public int getYear() {
         return year;
-    }
-
-    public void setYear(Date year) {
-        this.year = year;
     }
 
     public int getPage() {
         return page;
     }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
-
     public int getPegi() {
         return pegi;
     }
-
-    public void setPegi(int pegi) {
-        this.pegi = pegi;
-    }
-
     public float getPrice() {
         return price;
     }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
-
     public float getCurrentPrice() {
         return currentPrice;
     }
-
-    public void setCurrentPrice(float currentPrice) {
-        this.currentPrice = currentPrice;
-    }
-
     public boolean isSell() {
         return sell;
     }
-
-    public void setSell(boolean sell) {
-        this.sell = sell;
-    }
-
     public int getNumBorrow() {
         return numBorrow;
-    }
-
-    public void setNumBorrow(int numBorrow) {
-        this.numBorrow = numBorrow;
     }
 }
