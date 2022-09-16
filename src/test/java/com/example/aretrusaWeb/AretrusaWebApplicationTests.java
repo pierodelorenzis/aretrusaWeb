f32bcd84e82c240559e54bd81156709787854b92
pierodelorenzis <delorenzispiero@gmail.com>
2022-09-16 09:47:40 +0200

    Initial commit

---
+++ src/test/java/com/example/aretrusaWeb/AretrusaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aretrusaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AretrusaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
